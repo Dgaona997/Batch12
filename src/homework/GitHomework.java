d7607bce4756eb32c1f364d851b072c5c8d1d84a
danie <danie@DESKTOP-UA8MPDR.mynetworksettings.com>
2024-09-10 21:15:20 -0400

    Removed code

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,33 +2,6 @@ package homework;
 
 public class GitHomework {
 	
-	public static void main (String [] args) {
-		System.out.println("Batch 12: Git Class Lecture");
-		int age = 12;
-		String name = "John";
-		//This is comming from github
-		}
-	
-	public static void add (int a, int b) {
-		System.out.println(a + b);
-		}
-	
-	public static void subtract (int a, int b) {
-		System.out.println(a - b);
-		
-	}
-	public static void multiply(int a, int b) {
-		System.out.println(a * b);
-	}
-	
-	public static void divide (int a , int b) {
-		System.out.println(a/b);
-	}
 
-	//360 when you're in the mirror what do you see
-
-	// im so julia ah ah ah ah
-	
-	
 
 }
