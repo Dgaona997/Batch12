cc927ae33c3fb94c1df383cd36b58a588d21cb2f
Dgaona997 <gaonalopezdaniel@gmail.com>
2024-09-10 21:53:25 -0400

    Update GitHomework.java

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -5,4 +5,11 @@ public class GitHomework {
 	public void display() {
 		System.out.println("Daniel Gaona");
 	}
+
+	/**
+		this is coming from gitHub
+  		Java is fun
+  		git is Easy
+  		Api is not fun..
+	*/
 }
