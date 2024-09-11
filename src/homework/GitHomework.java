eaac6839fc270fb2a65fae9097b17ed3161a5f2c
danie <danie@DESKTOP-UA8MPDR.mynetworksettings.com>
2024-09-10 22:06:21 -0400

    Pushed

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -1,15 +1,5 @@
 package homework;
 
 public class GitHomework {
-	
-	public void display() {
-		System.out.println("Daniel Gaona");
-	}
 
-	/**
-		this is coming from gitHub
-  		Java is fun
-  		git is Easy
-  		Api is not fun..
-	*/
 }
