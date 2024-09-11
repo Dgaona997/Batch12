50b73e7c6f5000aa34f47fa3e3011817d66a29c7
danie <danie@DESKTOP-UA8MPDR.mynetworksettings.com>
2024-09-10 20:43:31 -0400

    Added divide method

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -17,5 +17,9 @@ public class GitHomework {
 	public static void multiply(int a, int b) {
 		System.out.println(a * b);
 	}
+	
+	public static void divide (int a , int b) {
+		System.out.println(a/b);
+	}
 
 }
